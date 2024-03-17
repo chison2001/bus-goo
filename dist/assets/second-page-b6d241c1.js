302166a9975ce90b124121c7af43ec9e10f8cef5
chison2001 <sonsonsonle9a1@gmail.com>
2024-03-17 12:31:16 +0700

    test deploy

---
+++ dist/assets/second-page-b6d241c1.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o as c,c as o}from"./index-e6b68dc9.js";const n={};function r(t,a){return c(),o("div",null," xin chào đến với page 2 ")}const f=e(n,[["render",r]]);export{f as default};
